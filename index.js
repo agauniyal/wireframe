57434329fd3c29a96e1a885e4671f6c61c3ee685
frank hague <frankhaguemail@gmail.com>
2016-11-17 15:56:41 +0000

    separating the build files from the static files. Using express to handle routing and sass compilation

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 var express = require('express'),
+    sassMiddleware = require('node-sass-middleware'),
     path = require('path'),
     app = express(),
     port = 8888;
 
-/* basic configuration */
+
+
 app.set('views', path.join(__dirname, '/server/views'));
 app.set('view engine','jade');
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(app.router);
 
-/*set basic routing for static files */
-app.use(express.static( path.join(__dirname, '/public') ));
+// notice that the following line has been removed
+// app.use(express.static(__dirname + '/public'));
+
+// adding the sass middleware
+
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/min'  // Where prefix is at <link rel="stylesheets" href="min/style.css"/>
+}));
+
+// The static middleware must come after the sass middleware
+app.use(express.static( path.join( __dirname, 'public' ) ) );
+
+
+
+
+app.get('/viewport/:viewport', function (req, res) {
+    // res.send('viewports/' + req.params.viewport)
+    res.render('viewports/' + req.params.viewport, {
+        title: 'Wireframes made easy'
+    })
+})
+
 
 app.listen(port,function() {
     console.log('Wireframes is running on  '+port+' port');
 });
+
+
+
+
+
+
