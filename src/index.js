6f3971a6d4aa7af328ea1e40032b512206fd9edc
Eric Quanz <eric.quanz@gmail.com>
2017-10-20 16:23:02 +0200

    Add normalize.css dependency, update page title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css'
+import './sass/main.css'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
